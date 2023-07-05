3494c8847393ef6bb4ec2016e7ed2c3d49745f60
Aamena Rangwala <aamena.r@yahoo.com>
2023-07-05 20:46:13 +0530

    .

--- bootstrap-assesssment/script.js
+++ bootstrap-assesssment/script.js
@@ -1,11 +1,15 @@
 // $document.ready(function(){
   
   $(document).click(function(e){
-    if(e.target.classList.contains('gallery-item')){
+    //e.target.classList.contains('gallery-item')  //js method
+    if($(e.target).hasClass('gallery-item')){     // convert in jquery           
       const src = $(e.target).attr('src');
+      const alt = $(e.target).attr('alt');
+      console.log(alt)
     
-      // $('.modal-img').src = srcz;  //this syntax did not worked
+      // $('.modal-img').src = srcz;     //this syntax did not worked
       $('.modal-img').attr('src', src); // this syntax worked
+      $('.modal-img').attr('alt', alt); 
   
       var myModal = new bootstrap.Modal($('#gallery-modal'));
       myModal.show();
